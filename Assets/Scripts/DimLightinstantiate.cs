c6848d169d0fd1243740f0657cfa3ee160505a3b
Quincy <Quincy.Lewis@mail.citytech.cuny.edu>
2021-11-18 03:25:52 -0500

    Instantiate Script

---
+++ Assets/Scripts/DimLightinstantiate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimLightinstantiate : MonoBehaviour
+{
+    public GameObject[] prefabInstantiate;
+    //public GameObject ObjectToInstantiateON;
+
+    public Transform pos;
+
+    public int randomInt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnLightBall()
+    {
+        randomInt = Random.Range(0, prefabInstantiate.Length);
+        Instantiate(prefabInstantiate[randomInt], pos.position, pos.rotation);
+    }
+
+    
+
+    
+}
