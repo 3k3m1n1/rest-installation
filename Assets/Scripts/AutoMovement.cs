6f761dd8d87a0db05ca6701a2b92af1a4d072135
Quincy <Quincy.Lewis@mail.citytech.cuny.edu>
2021-12-13 20:30:33 -0500

    Finish Automated Movement for Escalator
    When you activate the trigger for automation, the line reaches its end point, manually check the script in inspector.

--- Assets/Scripts/AutoMovement.cs
+++ Assets/Scripts/AutoMovement.cs
@@ -1,23 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 
 public class AutoMovement : MonoBehaviour
 {
-    //Think of using Waypoints the next time you visit this script.
+    //public GameObject target;
 
+    public EditorPath followPath;
+
+    public int CurrentWayPointID = 0;
+    public float speed;
+    private float reachDistance = 1.0f;
+    public float rotationSpeed = 5.0f;
+    //private int lastIndex;
+    public string pathName;
+
+    Vector3 lastPos;
+    Vector3 currentPos;
+
+    public AutoTrigger Grind;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(followPath.pathOjs[CurrentWayPointID].position, transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, followPath.pathOjs[CurrentWayPointID].position, Time.deltaTime * speed);
+
+        if (distance <= reachDistance)
+        {
+            CurrentWayPointID++;
+            
+        }
+        
+        if (Grind.isGrinding)
+        {
+            if (followPath.pathOjs.Last())
+            {
+                GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = false;
+                GameObject.Find("FPSController").GetComponent<CharacterController>().enabled = false;
+                
+            }
+        }
+        
+        
+
+    }
+
+
+
+    
+    /*
     public GameObject target;
 
     public GameObject[] wayPoints;
 
-    public GameObject Trigger;
+    //public GameObject Trigger;
 
     public int currentPoint;
 
     public float speed;
 
-    public bool isGrinding;
+    public AutoTrigger Grind;
+
+    //public bool isGrinding;
 
     //public CharacterController CC;
     public Rigidbody rb;
@@ -28,6 +81,9 @@ public class AutoMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Grind.isGrinding = true;
+
+
     }
 
     // Update is called once per frame
@@ -38,12 +94,14 @@ public class AutoMovement : MonoBehaviour
 
     void MovingAlongWayPoints()
     {
-        if (isGrinding)
+        if (Grind.isGrinding)
         {
             //target.SetActive(false);
-            GameObject.Find("Player").GetComponent<FirstPersonController>().enabled = false;
-            GameObject.Find("Player").GetComponent<CharacterController>().enabled = false;
-            GameObject.Find("Player").GetComponent<Rigidbody>().isKinematic = false;
+            GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = false;
+            GameObject.Find("FPSController").GetComponent<CharacterController>().enabled = false;
+            //GameObject.Find("FPSController").GetComponent<Rigidbody>().isKinematic = true;
+            Debug.Log(Grind.isGrinding);
+            
             transform.position = Vector3.Lerp(Vector3.up, Vector3.forward, speed * Time.deltaTime);
             if (Vector3.Distance(transform.position, wayPoints[currentPoint].transform.position) < 1)
             {
@@ -54,6 +112,7 @@ public class AutoMovement : MonoBehaviour
         
     }
 
+    
     void OnTriggerEnter(Collider other)
     {
 
@@ -63,32 +122,7 @@ public class AutoMovement : MonoBehaviour
             isGrinding = true;
         }
 
-        /*
-        if (other.gameObject.tag == "StartTrigger")
-        {
-            
-            GameObject.Find("Player").GetComponent<FirstPersonController>().enabled = false;
-            GameObject.Find("Player").GetComponent<CharacterController>().enabled = false;
-            GameObject.Find("Player").GetComponent<Rigidbody>().isKinematic = true;
-            target.transform.Translate(Vector3.forward * Time.deltaTime);
-            target.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
-            Debug.Log("Start Trigger works");
-        }
-        */
+        
     }
-    /*
-    void OnTriggerExit(Collider col)
-    {
-        if (col.gameObject.tag == "ExitTrigger")
-        {
-            target.SetActive(true);
-            
-
-            
-
-            Debug.Log("Exit Trigger works");
-        }
-        
-        
-    } */
+    */
 }
---
+++ Assets/Scripts/AutoTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class AutoTrigger : MonoBehaviour
+{
+    //public AnimationClip Upward;
+    //public Animation anim;
+
+    public bool isGrinding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //anim = GetComponent<Animator>();
+        //anim.enabled = false;
+        //anim.playAutomatically = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("StartTrigger"))
+        {
+            //anim.enabled = true;
+            //anim.Play();
+            isGrinding = true;
+            GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = true;
+            GameObject.Find("FPSController").GetComponent<CharacterController>().enabled = true;
+        }
+        
+    }
+    /*
+    public void ExitAnimation()
+    {
+        anim.Stop();
+        transform.position = new Vector3(38.55f, 10.72f, -5.91f); 
+        
+
+    }
+    */
+}
